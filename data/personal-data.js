3844406a13d9d48a2a7f94f11c16adacc980ee42
Danny Heyes <63042730+DannyHeyes@users.noreply.github.com>
2022-01-04 14:38:33 +0100

    Delete data directory

--- data/personal-data.js
+++
@@ -1,7 +0,0 @@
-export const danny = {
-    header: "Hi, I'm Danny and I build clean, responsive websites",
-    about: "About Me",
-    profile: "I’m a front-end developer that is always looking for new, exciting oppotunities. I focus on writing semantic HTML, using modern CSS practices and writing clean JavaScript. When building bigger projects, I mostly use React, but I can adapt to whatever tools are required. I’m based in Berlin, Germany, but I’m happy working remotely. When I’m not coding, you’ll find me making coffee. I enjoy using different brewing techniques and practicing my latte art. Why not have a look at my work?",
-    contact: "I’d love to hear about what you’re working on and how I could assist. I’m always on the lookout for new and exciting projects to sink my teeth in to. Currently I am based in Berlin, and am open to work in the city, but I am interested in other opportunities as well.",
-    contact2: "I’m a hard-working and positive person who will always approach each task with can-do attitude and attention to detail. Please  feel free to check out my online profiles below and get in touch using the form."
-}
